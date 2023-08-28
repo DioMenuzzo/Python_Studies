4016a00cc2b07bcfa71438dc7be40c7b68f5922b
Dio Menuzzo <118781259+DioMenuzzo@users.noreply.github.com>
2023-08-28 17:57:10 -0300

    Update 217. Contains Duplicate.py

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -1,7 +1,7 @@
 """ 
-o-----------------------o
-|     TWO SUM (EASY)    |
-o-----------------------o
+o------------------------------o
+|   CONTAINS DUPLICATE (EASY)  |
+o------------------------------o
 
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
